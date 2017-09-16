3777605d0686b9a3118b62a456c29e2ca0cae871
Ian McDowell <me@ianmcdowell.net>
2017-09-16 13:32:34 -0700

    Removed storyboards because they are dumb

--- iOS/Hackathon/ViewController.swift
+++ iOS/Hackathon/ViewController.swift
@@ -12,7 +12,18 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
-    @IBOutlet var sceneView: ARSCNView!
+    private let sceneView: ARSCNView
+    
+    init() {
+        sceneView = ARSCNView(frame: .zero, options: nil)
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    override func loadView() {
+        self.view = sceneView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
