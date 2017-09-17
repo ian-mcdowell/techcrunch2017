efb93b91d5a29819acbf612f3f7e7b0cbf41af3e
Nathanael Hardy <nchardy1@gmail.com>
2017-09-17 00:50:29 -0700

    Changed startup controller

--- iOS/Hackathon/AppDelegate.swift
+++ iOS/Hackathon/AppDelegate.swift
@@ -16,6 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let storyboard = UIStoryboard(name: "main", bundle: nil)
+        
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            window.backgroundColor = UIColor.white
+            window.rootViewController = storyboard.instantiateViewController(withIdentifier: "MainScreen")
+            window.makeKeyAndVisible()
+        }
+        
         return true
     }
 
